9c64209d1cf0d454159d152dac8c1fbefb93a581
Jedsadaporn pannok <tikpop555@gmail.com>
2023-12-05 17:33:06 +0700

    login and sha256

--- TIKXDedit.c
+++ TIKXDedit.c
@@ -1,7 +1,8 @@
 #include<stdio.h>
-#include<string.h>
+#include <string.h>
 #include <stdlib.h>
 #include<time.h>
+#include <stdint.h>
 
 //Define the Worst case scenario number
 #define MAX_LINE_LENGTH 100
@@ -442,12 +443,163 @@ void appendpro(struct Product *products, char date[20]) {
 //--------------------------------------------- ดินแดนของ TIKPOPTV ---------------------------------------------//
 
 #define MAX_PASSWORD_LENGTH 50
+#define ROTRIGHT(word, bits) (((word) >> (bits)) | ((word) << (32 - (bits))))
+#define ROTLEFT(word, bits) (((word) << (bits)) | ((word) >> (32 - (bits))))
+#define CH(x, y, z) (((x) & (y)) ^ (~(x) & (z)))
+#define MAJ(x, y, z) (((x) & (y)) ^ ((x) & (z)) ^ ((y) & (z)))
+
+#define SHA256_BLOCK_SIZE 64
+#define SHA256_DIGEST_LENGTH 32
+
+typedef struct {
+    uint8_t data[64];
+    uint32_t datalen;
+    uint64_t bitlen;
+    uint32_t state[8];
+} SHA256_CTX;
+
+uint32_t sig0(uint32_t x) {
+    return (ROTRIGHT(x, 7) ^ ROTRIGHT(x, 18) ^ (x >> 3));
+}
+
+uint32_t sig1(uint32_t x) {
+    return (ROTRIGHT(x, 17) ^ ROTRIGHT(x, 19) ^ (x >> 10));
+}
+
+uint32_t SIG0(uint32_t x) {
+    return (ROTRIGHT(x, 2) ^ ROTRIGHT(x, 13) ^ ROTRIGHT(x, 22));
+}
+
+uint32_t SIG1(uint32_t x) {
+    return (ROTRIGHT(x, 6) ^ ROTRIGHT(x, 11) ^ ROTRIGHT(x, 25));
+}
+
+const uint32_t k[] = {
+    0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
+    0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
+    0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc, 0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
+    0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7, 0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967,
+    0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13, 0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85,
+    0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3, 0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,
+    0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5, 0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
+    0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2
+};
+
+void sha256_transform(SHA256_CTX *ctx, const uint8_t data[]) {
+    uint32_t a, b, c, d, e, f, g, h, i, j, t1, t2, m[64];
+
+    for (i = 0, j = 0; i < 16; ++i, j += 4)
+        m[i] = (data[j] << 24) | (data[j + 1] << 16) | (data[j + 2] << 8) | (data[j + 3]);
+
+    for (; i < 64; ++i)
+        m[i] = sig1(m[i - 2]) + m[i - 7] + sig0(m[i - 15]) + m[i - 16];
+
+    a = ctx->state[0];
+    b = ctx->state[1];
+    c = ctx->state[2];
+    d = ctx->state[3];
+    e = ctx->state[4];
+    f = ctx->state[5];
+    g = ctx->state[6];
+    h = ctx->state[7];
+
+    for (i = 0; i < 64; ++i) {
+        t1 = h + SIG1(e) + CH(e, f, g) + k[i] + m[i];
+        t2 = SIG0(a) + MAJ(a, b, c);
+        h = g;
+        g = f;
+        f = e;
+        e = d + t1;
+        d = c;
+        c = b;
+        b = a;
+        a = t1 + t2;
+    }
+
+    ctx->state[0] += a;
+    ctx->state[1] += b;
+    ctx->state[2] += c;
+    ctx->state[3] += d;
+    ctx->state[4] += e;
+    ctx->state[5] += f;
+    ctx->state[6] += g;
+    ctx->state[7] += h;
+}
+
+void sha256_init(SHA256_CTX *ctx) {
+    ctx->datalen = 0;
+    ctx->bitlen = 0;
+    ctx->state[0] = 0x6a09e667;
+    ctx->state[1] = 0xbb67ae85;
+    ctx->state[2] = 0x3c6ef372;
+    ctx->state[3] = 0xa54ff53a;
+    ctx->state[4] = 0x510e527f;
+    ctx->state[5] = 0x9b05688c;
+    ctx->state[6] = 0x1f83d9ab;
+    ctx->state[7] = 0x5be0cd19;
+}
+
+void sha256_update(SHA256_CTX *ctx, const uint8_t data[], size_t len) {
+    for (size_t i = 0; i < len; ++i) {
+        ctx->data[ctx->datalen] = data[i];
+        ctx->datalen++;
+        if (ctx->datalen == 64) {
+            sha256_transform(ctx, ctx->data);
+            ctx->bitlen += 512;
+            ctx->datalen = 0;
+        }
+    }
+}
+
+void sha256_final(SHA256_CTX *ctx, uint8_t hash[]) {
+    uint32_t i = ctx->datalen;
+
+    if (ctx->datalen < 56) {
+        ctx->data[i++] = 0x80;
+        while (i < 56)
+            ctx->data[i++] = 0x00;
+    } else {
+        ctx->data[i++] = 0x80;
+        while (i < 64)
+            ctx->data[i++] = 0x00;
+        sha256_transform(ctx, ctx->data);
+        memset(ctx->data, 0, 56);
+    }
+
+    ctx->bitlen += ctx->datalen * 8;
+    ctx->data[63] = ctx->bitlen;
+    ctx->data[62] = ctx->bitlen >> 8;
+    ctx->data[61] = ctx->bitlen >> 16;
+    ctx->data[60] = ctx->bitlen >> 24;
+    ctx->data[59] = ctx->bitlen >> 32;
+    ctx->data[58] = ctx->bitlen >> 40;
+    ctx->data[57] = ctx->bitlen >> 48;
+    ctx->data[56] = ctx->bitlen >> 56;
+    sha256_transform(ctx, ctx->data);
+
+    for (i = 0; i < 4; ++i) {
+        hash[i] = (ctx->state[0] >> (24 - i * 8)) & 0x000000ff;
+        hash[i + 4] = (ctx->state[1] >> (24 - i * 8)) & 0x000000ff;
+        hash[i + 8] = (ctx->state[2] >> (24 - i * 8)) & 0x000000ff;
+        hash[i + 12] = (ctx->state[3] >> (24 - i * 8)) & 0x000000ff;
+        hash[i + 16] = (ctx->state[4] >> (24 - i * 8)) & 0x000000ff;
+        hash[i + 20] = (ctx->state[5] >> (24 - i * 8)) & 0x000000ff;
+        hash[i + 24] = (ctx->state[6] >> (24 - i * 8)) & 0x000000ff;
+        hash[i + 28] = (ctx->state[7] >> (24 - i * 8)) & 0x000000ff;
+    }
+}
+
+void sha256(const uint8_t *data, size_t len, uint8_t hash[SHA256_DIGEST_LENGTH]) {
+    SHA256_CTX ctx;
+    sha256_init(&ctx);
+    sha256_update(&ctx, data, len);
+    sha256_final(&ctx, hash);
+}
 
 struct AdminData {
     char username[MAX_PASSWORD_LENGTH];
     char password[MAX_PASSWORD_LENGTH];
 };
-
 
 void writeAdminDataToBinaryFile(struct AdminData *adminData) {
     FILE *file = fopen("AdminDB.dat", "wb");
@@ -473,6 +625,98 @@ void readAdminDataFromBinaryFile(struct AdminData *adminData) {
     fclose(file);
 }
 
+
+void addAdmin() {
+    struct AdminData admin;
+
+    printf("Enter admin username: ");
+    scanf("%s", admin.username);
+
+    printf("Enter admin password: ");
+    scanf("%s", admin.password);
+
+    // Hash the password before storing it
+    uint8_t hashedPassword[SHA256_DIGEST_LENGTH];
+    sha256((uint8_t *)admin.password, strlen(admin.password), hashedPassword);
+
+    // Copy the hashed password to the admin data
+    memcpy(admin.password, hashedPassword, SHA256_DIGEST_LENGTH);
+
+    FILE *file = fopen("AdminDB.dat", "ab");
+    if (!file) {
+        printf("Error opening file for writing\n");
+        return;
+    }
+
+    fwrite(&admin, sizeof(struct AdminData), 1, file);
+
+    fclose(file);
+
+    printf("Admin added successfully.\n");
+}
+
+int login() {
+    char enteredUsername[MAX_PASSWORD_LENGTH];
+    char enteredPassword[MAX_PASSWORD_LENGTH];
+
+    struct AdminData adminData;
+    readAdminDataFromBinaryFile(&adminData);
+
+    printf("Enter username: ");
+    scanf("%s", enteredUsername);
+
+    printf("Enter password: ");
+    scanf("%s", enteredPassword);
+
+    if (strcmp(enteredUsername, adminData.username) == 0 && strcmp(enteredPassword, adminData.password) == 0) {
+        return 1; // คืนค่า 1 เมื่อเข้าสู่ระบบสำเร็จ
+    } else {
+        return 0; // คืนค่า 0 เมื่อเข้าสู่ระบบไม่สำเร็จ
+    }
+}
+
+int adminLogin() {
+    struct AdminData admin;
+    char username[MAX_PASSWORD_LENGTH];
+    char password[MAX_PASSWORD_LENGTH];
+
+    printf("Enter username: ");
+    scanf("%s", username);
+
+    printf("Enter password: ");
+    scanf("%s", password);
+
+    FILE *file = fopen("AdminDB.dat", "rb");
+    if (!file) {
+        printf("Error opening file for reading\n");
+        return 0;
+    }
+
+    int found = 0;
+    while (fread(&admin, sizeof(struct AdminData), 1, file)) {
+        if (strcmp(admin.username, username) == 0) {
+            // Hash the entered password for comparison
+            uint8_t enteredPasswordHash[SHA256_DIGEST_LENGTH];
+            sha256((uint8_t *)password, strlen(password), enteredPasswordHash);
+
+            // Compare the hashed password with stored hashed password
+            if (memcmp(admin.password, enteredPasswordHash, SHA256_DIGEST_LENGTH) == 0) {
+                found = 1;
+                break;
+            }
+        }
+    }
+
+    fclose(file);
+
+    if (found) {
+        printf("Login successful!\n");
+        return 1;
+    } else {
+        printf("Login failed. Invalid username or password.\n");
+        return 0;
+    }
+}
 
 
 void updateCSV(struct Product *products, int row) {
@@ -512,19 +756,7 @@ void updateBinary(struct Product *products, int row) {
 
 
 void editTransaction(struct Product *products, int row) {
-    char enteredUsername[MAX_PASSWORD_LENGTH];
-    char enteredPassword[MAX_PASSWORD_LENGTH];
-
-    struct AdminData adminData;
-    readAdminDataFromBinaryFile(&adminData);
-
-    printf("Enter username: ");
-    scanf("%s", enteredUsername);
-
-    printf("Enter password: ");
-    scanf("%s", enteredPassword);
-     if (strcmp(enteredUsername, adminData.username) == 0 && strcmp(enteredPassword, adminData.password) == 0) {
-        printf("Login successful!\n");
+    if (adminLogin()) { 
         int selection;
 
         printf("Products List:\n");
@@ -669,6 +901,9 @@ int main() {
     else if(select == 4) {
         printf("Selected option 4. Displaying all products...\n");
         displayAll(products, &row);
+    }
+    else if(select == 5) {
+        adminLogin();
     }
     return 0;
 }
