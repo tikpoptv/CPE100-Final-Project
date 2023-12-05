61e289513031e74ae24826b81d80febbd6276a4b
Ryu <132838860+Phattharyu@users.noreply.github.com>
2023-12-05 22:28:33 +0700

    Last Commit

--- CODE/trybin.c
+++ CODE/trybin.c
@@ -1,14 +1,15 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <time.h>
-#include <stdint.h>
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<time.h>
+#include<stdint.h>
 
 //Define the Worst case scenario number
 #define MAX_LINE_LENGTH 100
 #define MAX_PRODUCTS 100
 #define MAX_Product_ID 20
 
+//Struct
 struct Product {
     char inout[MAX_LINE_LENGTH];
     char date[MAX_LINE_LENGTH];
@@ -20,6 +21,7 @@ struct Product {
     int price;
 };
 
+//<-------------------Display funtions------------------------->
 //display when start of the process
 void displayMenu() {
     printf("<== Select features: ==>\n");
@@ -29,6 +31,7 @@ void displayMenu() {
     printf("4. Display All Products\n");
     printf("0. Close Program\n");
 }
+
 //Display choices for summary
 void displaySummary() {
     printf("<== Select features: ==>\n");
@@ -36,6 +39,7 @@ void displaySummary() {
     printf("2. Summary of sales right now\n");
     printf("0. Close Program\n");
 }
+
 //Display choices of Ice Cream Menu
 void displayIceCreamMenu() {
     printf("<== Select features: ==>\n");
@@ -50,6 +54,7 @@ void displayIceCreamMenu() {
     printf("0. Close Program\n");
 }
 
+//To find the number of rows in the file
 int findNumberOfRecords(const char *filename, size_t recordSize) {
     FILE *file = fopen(filename, "rb");
     if (!file) {
@@ -67,7 +72,7 @@ int findNumberOfRecords(const char *filename, size_t recordSize) {
 
     return numberOfRecords;
 }
-
+//To read the binary file from the path
 void readFromBinaryFile(struct Product *products, int *count, const char *filename) {
     FILE *file = fopen(filename, "rb");
     if (!file) {
@@ -79,7 +84,7 @@ void readFromBinaryFile(struct Product *products, int *count, const char *filena
 
     fclose(file);
 }
-
+//Displaying all transactions in the file
 void displayAll(struct Product *products, int *row) {
     printf("Displaying all products:\n");
     printf("No. In/Out Date       Expired Date Time Product ID Name         Amount Price\n");
@@ -91,13 +96,13 @@ void displayAll(struct Product *products, int *row) {
             products[i].quantity, products[i].price);
     }
 }
-//Check the date is valid or not
+//Check the year is a leap year or not
 int isLeapYear(int year) {
     // Leap year is divisible by 4
     // Exception: Years divisible by 100 are not leap years, unless they are divisible by 400
     return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
 }
-
+//Check the date is valid or not
 int isValidDate(const char *date) {
     int day, month, year;
     if (sscanf(date, "%d/%d/%d", &day, &month, &year) != 3) {
@@ -165,14 +170,14 @@ void Display(struct Product *products, int *row) {
     }
 
     struct Product sum[cnt];
-
+    //set the price and quantity to be zero
     for(int i=0;i<cnt;i++ ) {
         sum[i].price=0;
         sum[i].quantity=0.0;
     }
 
     struct Product sum2[cnt];
-
+    //set the price and quantity to be zero
     for(int i=0;i<cnt;i++ ) {
         sum2[i].price=0;
         sum2[i].quantity=0.0;
@@ -189,6 +194,7 @@ void Display(struct Product *products, int *row) {
     displaySummary();//display choices of display summary
     int want;
     scanf("%d",&want);
+    //Show the amount left in stock
     if(want == 1) {
         for(int i=0;i<cnt;i++) {
             for(int j=0;j<*row;j++) {
@@ -209,7 +215,7 @@ void Display(struct Product *products, int *row) {
             printf("%-12s\t%-12s\t%.2lf\t%d\n",sum[i].productID,sum[i].name,sum[i].quantity,sum[i].price);
         }
     }
-
+    //Show the summary of sales
     else if(want == 2) {
         int moneynow = 0;
         for(int i=0;i<cnt;i++) {
@@ -241,15 +247,17 @@ void Display(struct Product *products, int *row) {
 
 //<------------------Update a product in our file-------------------------->
 void appendpro(struct Product *products) {
-    FILE *file = fopen("../Database/IceCreambin.dat", "ab");  // Open the binary file for appending in binary mode
+    // Open the binary file for append in binary mode
+    FILE *file = fopen("IceCreambin.dat", "ab");  
 
     if (!file) {
         printf("Error opening file for appending\n");
         return;
     }
 
-    struct Product update;
+    struct Product update;//to keep the information in update and add to the file
     char date[20];
+    //input the date
     printf("What's Date is today ?(Write in DD/MM/YYYY format) : ");
     scanf("%s",date);
     while (isValidDate(date) == 0) {
@@ -257,9 +265,10 @@ void appendpro(struct Product *products) {
         scanf("%s",date);
     }
     char a[9];
+    //input the transaction type
     printf("In or Out or Cancel: ");
     scanf("%s", a);
-
+    //In
     if (strcmp(a, "In") == 0) {
         strcpy(update.inout, a);
         strcpy(update.date,date);
@@ -282,7 +291,9 @@ void appendpro(struct Product *products) {
 
         // Write the new product to the end of the binary file
         fwrite(&update, sizeof(struct Product), 1, file);
-    } else if (strcmp(a, "Out") == 0) {
+    } 
+    //Out
+    else if (strcmp(a, "Out") == 0) {
         displayIceCreamMenu();
         int inp;
         printf("Input your Menu : ");
@@ -479,9 +490,12 @@ void appendpro(struct Product *products) {
             update.price = 9;
             fwrite(&update, sizeof(struct Product), 1, file);
         }
-    } else if (strcmp(a, "Cancel") == 0) {
+    } 
+    //Cancel
+    else if (strcmp(a, "Cancel") == 0) {
         printf("Canceled!!!\n");
-    } else {
+    } 
+    else {
         printf("Error Input! Please Try Again\n");
         appendpro(products);
     }
@@ -490,13 +504,13 @@ void appendpro(struct Product *products) {
 }
 
 //--------------------------------------------- Start of TIKPOPTV ---------------------------------------------//
-
+//Editing and Deleting the transaction in FILE
 #define MAX_PASSWORD_LENGTH 50
 #define ROTRIGHT(word, bits) (((word) >> (bits)) | ((word) << (32 - (bits))))
 #define ROTLEFT(word, bits) (((word) << (bits)) | ((word) >> (32 - (bits))))
 #define CH(x, y, z) (((x) & (y)) ^ (~(x) & (z)))
 #define MAJ(x, y, z) (((x) & (y)) ^ ((x) & (z)) ^ ((y) & (z)))
-
+//Using SHA256 to keep the password
 #define SHA256_BLOCK_SIZE 64
 #define SHA256_DIGEST_LENGTH 32
 
@@ -651,7 +665,7 @@ struct AdminData {
 };
 
 void writeAdminDataToBinaryFile(struct AdminData *adminData) {
-    FILE *file = fopen("../Database/AdminDB.dat", "wb");
+    FILE *file = fopen("AdminDB.dat", "wb");
     if (!file) {
         printf("Error opening file for writing\n");
         return;
@@ -663,7 +677,7 @@ void writeAdminDataToBinaryFile(struct AdminData *adminData) {
 }
 
 void readAdminDataFromBinaryFile(struct AdminData *adminData) {
-    FILE *file = fopen("../Database/AdminDB.dat", "rb");
+    FILE *file = fopen("AdminDB.dat", "rb");
     if (!file) {
         printf("Error opening file for reading\n");
         return;
@@ -691,7 +705,7 @@ void addAdmin() {
     // Copy the hashed password to the admin data
     memcpy(admin.password, hashedPassword, SHA256_DIGEST_LENGTH);
 
-    FILE *file = fopen("../Database/AdminDB.dat", "ab");
+    FILE *file = fopen("AdminDB.dat", "ab");
     if (!file) {
         printf("Error opening file for writing\n");
         return;
@@ -735,7 +749,7 @@ int adminLogin() {
     printf("Enter password: ");
     scanf("%s", password);
 
-    FILE *file = fopen("../Database/AdminDB.dat", "rb");
+    FILE *file = fopen("AdminDB.dat", "rb");
     if (!file) {
         printf("Error opening file for reading\n");
         return 0;
@@ -786,7 +800,7 @@ void updateCSV(struct Product *products, int row) {
 }
 
 void updateBinary(struct Product *products, int row) {
-    FILE *binaryFile = fopen("../Database/IceCreambin.dat", "wb");
+    FILE *binaryFile = fopen("IceCreambin.dat", "wb");
 
     if (binaryFile == NULL) {
         printf("Error opening binary file for writing.\n");
@@ -880,7 +894,7 @@ void editTransaction(struct Product *products, int row) {
 }
 
 //--------------------------------------------- End Of TIKPOPTV ---------------------------------------------//
-
+//Check the near expiration items in the stock
 void DisplayNearExpiration(struct Product *products, int *row) {
     printf("Items Near Expiration (within 30 days):\n");
     printf("Product ID\tName\t       Expired Date\n");
@@ -913,19 +927,106 @@ void DisplayNearExpiration(struct Product *products, int *row) {
         }
     }
 }
+//Displaying Out of stock
+void DisplayOutofStock(struct Product *products, int *row) {
+    printf("Items Near Out of stock :\n");
+    char *strings[MAX_Product_ID];
+    char *name[MAX_Product_ID];
+    int cnt = 0;
+    
+    // Initialize pointers
+    for (int i = 0; i < MAX_Product_ID; i++) {
+        strings[i] = malloc(MAX_LINE_LENGTH);  // Adjust MAX_LINE_LENGTH according to your needs
+        name[i] = malloc(MAX_LINE_LENGTH);
+        if (strings[i] == NULL) {
+            // Handle memory allocation failure
+            exit(EXIT_FAILURE);
+        }
+    }
 
+    // Copy unique strings
+    for (int i = 0; i < *row; i++) {
+        int isDuplicate = 0;
+        for (int j = 0; j < cnt; j++) {
+            if (strcmp(strings[j], products[i].productID) == 0) {
+                isDuplicate = 1;
+                break;
+            }
+        }
+        if (!isDuplicate) {
+            strcpy(strings[cnt], products[i].productID);
+            strcpy(name[cnt],products[i].name);
+            cnt++;
+        }
+    }
 
+    struct Product sum[cnt];
+    //set the price and quantity to be zero
+    for(int i=0;i<cnt;i++ ) {
+        sum[i].price=0;
+        sum[i].quantity=0.0;
+    }
+    for (int i = 0; i < cnt; i++) {
+        strcpy(sum[i].productID, strings[i]);
+        strcpy(sum[i].name,name[i]);
+    }
+    for(int i=0;i<cnt;i++) {
+        for(int j=0;j<*row;j++) {
+            if(strcmp(sum[i].productID,products[j].productID) == 0) {
+                if(strcmp(products[j].inout, "In") == 0) {
+                    sum[i].price += products[j].price;
+                    sum[i].quantity += products[j].quantity;
+                }
+                else if(strcmp(products[j].inout, "Out") == 0) {
+                    sum[i].price -= products[j].price;
+                    sum[i].quantity -= products[j].quantity;
+                }
+            }
+        }
+    }
+    int count=0;
+    for(int i=0;i<*row;i++) {
+        if(strcmp(sum[i].name,"Chocolate")==0 && sum[i].quantity <= 3) {
+            printf("Chocolate is low on stock Amount left : %.2lf\n",sum[i].quantity);
+            count++;
+        }  
+        else if(strcmp(sum[i].name,"Matcha")==0 && sum[i].quantity <= 3) {
+            printf("Matcha is low on stock Amount left : %.2lf\n",sum[i].quantity);
+            count++;
+        }  
+        else if(strcmp(sum[i].name,"Strawberry")==0 && sum[i].quantity <= 3) {
+            printf("Strawberry is low on stock! Amount left : %.2lf\n",sum[i].quantity);
+            count++;
+        }  
+        else if(strcmp(sum[i].name,"Cookies & Cream")==0 && sum[i].quantity <= 3) {
+            printf("Cookies & Cream is low on stock! Amount left : %.2lf\n",sum[i].quantity);
+            count++;
+        }  
+        else if(strcmp(sum[i].name,"Cone")==0 && sum[i].quantity <= 3) {
+            printf("Cone is low on stock! Amount left : %.2lf\n",sum[i].quantity);
+            count++;
+        }  
+        else if(strcmp(sum[i].name,"Cup")==0 && sum[i].quantity <= 3) {
+            printf("Cup is low on stock! Amount left : %.2lf\n",sum[i].quantity);
+            count++;
+        } 
+    }
+    if(count == 0) printf("None\n");
+}
 
 int main() {
     int row=0,select;
     struct Product products[MAX_PRODUCTS];
     size_t recordsize = sizeof(struct Product);
-    row = findNumberOfRecords("../Database/IceCreambin.dat",recordsize);
-    readFromBinaryFile(products,&row,"../Database/IceCreambin.dat");
+    row = findNumberOfRecords("IceCreambin.dat",recordsize);
+    
+    readFromBinaryFile(products,&row,"IceCreambin.dat");
     system("clear|| cls");
     DisplayNearExpiration(products,&row);
+    DisplayOutofStock(products,&row);
     displayMenu();
     scanf("%d",&select);
+
     if(select == 2) {
         appendpro(products);
         main();
